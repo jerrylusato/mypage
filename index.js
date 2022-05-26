83a900d1fe7a482266a7aa4f50169f4745d301ff
jerrylusato <jeremiahlusato@gmail.com>
2022-05-26 18:07:02 +0300

    fix index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 const mongoose = require('mongoose')
 const express = require('express')
 const router = require('./router')
-const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const app = express()
 
 app.set('view-engine', 'ejs')
 
 app.use('/', router)
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => app.listen(process.env.PORT))
-  .then(() => console.log('Listening...'))
-  .catch(err => console.log(err));
+.connect(process.env.MONGO_URI)
+.then(() => app.listen(process.env.PORT))
+.then(() => console.log('Listening...'))
+.catch(err => console.log(err))
